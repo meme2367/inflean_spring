c0fa12c03a01b70f23d04ff9b7421b5d55fc1462
meme2367 <meme91322367@gmail.com>
2021-07-26 14:47:00 +0900

    feat : AppConfig 생성

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -2,10 +2,12 @@ package hello.core.order;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -19,7 +21,7 @@ public class OrderServiceTest {
   void order() {
 
     Long memberId = 1L;
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemberRepository memberRepository = new MemoryMemberRepository();
     DiscountPolicy discountPolicy = new FixDiscountPolicy();
     MemberService memberService = new MemberServiceImpl(memberRepository);
     OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
