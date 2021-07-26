17bf7180f95ab2e9c4fc3bc092297b8053821ac1
meme2367 <meme91322367@gmail.com>
2021-07-26 14:23:16 +0900

    feat : DIP 위반 주문하기

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,11 +2,13 @@ package hello.core.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
   @Test
+  @DisplayName("회원가입")
   void join() {
     //given
     MemberService memberService = new MemberServiceImpl();
---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+  @Test
+  @DisplayName("주문하기")
+  void order() {
+
+    Long memberId = 1L;
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    Member member = new Member(memberId, "memberA", Grade.VIP);
+    memberService.join(member);
+    Order itemA = orderService.createOrder(memberId, "itemA", 10000);
+
+    System.out.println(itemA);
+    System.out.println(itemA.calculatePrice());
+  }
+
+}
