943b3eb95d5d6058b50ed8e6326c77be585ef400
meme2367 <meme91322367@gmail.com>
2021-07-27 06:31:15 +0900

    feat : assertThrows

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,7 +1,13 @@
 package hello.core.beanfind;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -23,5 +29,19 @@ public class ApplicationContextInfoTest {
     }
   }
 
+  @Test
+  @DisplayName("빈 조회")
+  void findBeanByName() {
+    MemberService memberService = ac.getBean(MemberServiceImpl.class);
+    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+  }
+
+  @Test
+  @DisplayName("빈 이름으로 조회X")
+  void findBeanByNameX() {
+
+    assertThrows(NoSuchBeanDefinitionException.class, () ->
+        ac.getBean("xxxxx", MemberService.class));
+  }
 
 }
