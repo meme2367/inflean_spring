57ca334b1faf8b2eecfa2dda5a16d38456d0ee12
meme2367 <meme91322367@gmail.com>
2021-08-03 18:10:16 +0900

    feat : Multiply code

--- core/src/test/java/toby3/calculator/CalcSumTest.java
+++ core/src/test/java/toby3/calculator/CalcSumTest.java
@@ -3,16 +3,30 @@ package toby3.calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalcSumTest {
 
+  Calculator calculator;
+  String path;
+
+  @BeforeEach
+  public void setUp() {
+    this.calculator = new Calculator(new FileReadTemplate());
+    this.path = getClass().getResource("/numbers.txt").getPath();
+  }
+
   @Test
   public void sumOfNumbers() throws IOException {
-    FileReadTemplate fileReadTemplate = new FileReadTemplate();
-    Calculator calculator = new Calculator(fileReadTemplate);
-    int sum = calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
+    int sum = calculator.calcSum(this.path);
     assertThat(sum).isEqualTo(10);
+  }
+
+  @Test
+  public void multiplyOfNumbers() throws IOException {
+    int mul = calculator.calcMultiply(this.path);
+    assertThat(mul).isEqualTo(24);
   }
 
 
--- core/src/test/java/toby3/calculator/Calculator.java
+++ core/src/test/java/toby3/calculator/Calculator.java
@@ -1,6 +1,5 @@
 package toby3.calculator;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Calculator {
@@ -12,16 +11,28 @@ public class Calculator {
   }
 
   public int calcSum(String path) throws IOException {
-    
+
     BufferedReaderCallBack bufferedReaderCallBack = (br) -> {
       Integer sum = 0;
       String line = null;
-      while((line = br.readLine()) != null) {
+      while ((line = br.readLine()) != null) {
         sum += Integer.valueOf(line);
       }
       return sum;
     };
 
+    return fileReadTemplate.fildReadTemplate(path, bufferedReaderCallBack);
+  }
+
+  public int calcMultiply(String path) throws IOException {
+    BufferedReaderCallBack bufferedReaderCallBack = (br) -> {
+      Integer mul = 1;
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        mul *= Integer.valueOf(line);
+      }
+      return mul;
+    };
     return fileReadTemplate.fildReadTemplate(path, bufferedReaderCallBack);
   }
 }
