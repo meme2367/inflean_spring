23c2cf3131d9346189388888c056d4cdca5f4789
meme2367 <meme91322367@gmail.com>
2021-08-05 10:30:46 +0900

    feat : OrderService template callBack

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +22,29 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
     Member member = memberRepository.findById(memberId);
     int discountPrice = discountPolicy.discount(member, itemPrice);
+
+    /* template callback
+    Member member = memberRepository.findById(memberId);
+    int discountPrice = template(member, itemPrice, price -> price * 10 / 100);
+    */
     return new Order(memberId, itemName, itemPrice, discountPrice);
+
+  }
+
+
+  public int template(Member member, int price, DiscountCallBack discountCallBack) {
+    int count = 0;
+    if (member.getGrade() == Grade.VIP) {
+      count = discountCallBack.discount(price);
+    }
+
+    return count;
+  }
+
+  private interface DiscountCallBack {
+    int discount(int price);
   }
 }
