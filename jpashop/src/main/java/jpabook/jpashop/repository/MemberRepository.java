14713761005db8404d965473eeed9db2a6063b57
meme2367 <meme91322367@gmail.com>
2021-08-11 22:48:40 +0900

    feat : member repository  추가

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
@@ -18,5 +19,16 @@ public class MemberRepository {
 
   public Member find(Long id) {
     return em.find(Member.class, id);
+  }
+
+  public List<Member> findAll() {
+    return em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+  }
+
+  public List<Member> findByName(String name) {
+    return em.createQuery("select m from Member m where m.name = :name",Member.class)
+        .setParameter("name", name)
+        .getResultList();
   }
 }
