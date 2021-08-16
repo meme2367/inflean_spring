6e6f5ec860f7bbdacbd43ff4261c651017edd3b6
meme2367 <meme91322367@gmail.com>
2021-08-16 13:56:21 +0900

    feat : 기본 JPA 인강  초기 설정

--- autowired/src/main/java/com/example/autowired/AppRunner.java
+++ autowired/src/main/java/com/example/autowired/AppRunner.java
@@ -1,2 +1,27 @@
-package com.example.autowired;public class AppRunner {
+package com.example.autowired;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+  @Autowired
+  ResourceLoader resourceLoader;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    Resource resource = resourceLoader.getResource("classpath:text.txt");
+    System.out.println(resource.exists());
+    System.out.println(resource.getDescription());
+    System.out.println(Files.readString(Path.of(resource.getURI())));
+  }
 }
+
+
--- autowired/src/main/java/com/example/autowired/AutowiredApplication.java
+++ autowired/src/main/java/com/example/autowired/AutowiredApplication.java
@@ -2,12 +2,14 @@ package com.example.autowired;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class AutowiredApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AutowiredApplication.class, args);
     }
-
 }
